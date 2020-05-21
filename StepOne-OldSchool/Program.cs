8924f49096a6d3c372521660b1398f0a0d2fc5fe
Luke Merrett <luke.merrett@just-eat.com>
2020-05-21 09:25:20 +0100

    Bootstrapped solution

---
+++ StepOne-OldSchool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StepOne_OldSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ StepThree-DI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StepThree_DI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
